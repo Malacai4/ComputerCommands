fc28b09e09efcef02915b7341a23d07dbb9428bd
Malacai4 <42919107+Malacai4@users.noreply.github.com>
2020-05-11 19:30:17 -0500

    Update ComputerCommands.py

--- ComputerCommands.py
+++ ComputerCommands.py
@@ -1,63 +1,61 @@
 # Additional modules
 import os
 import socket
+import platform
 
-# Computer Object
-class Computer:
-    def __init__(self, OS='Windows'):
-        self.OS = OS
+OS = platform.system()
 
 # Shutdown computer
-    def shutdown(self):
-        if self.OS == "Windows":
-            os.system("shutdown /s")
+def shutdown():
+    if OS == "Windows":
+        os.system("shutdown /s")
 
-        elif self.OS == "MacOS" or self.OS == "Linux":
-            os.system("sudo shutdown -h now")
+    elif OS == "Darwin" or OS == "Linux":
+        os.system("sudo shutdown -h now")
 
 # Reboot computer
-    def reboot(self):
-        if self.OS == "Windows":
-            os.system("shutdown /r")
+def reboot():
+    if OS == "Windows":
+        os.system("shutdown /r")
 
-        elif self.OS == "MacOS" or self.OS == "Linux":
-            os.system("sudo shutdown -r now")
+    elif OS == "Darwin" or OS == "Linux":
+        os.system("sudo shutdown -r now")
 
 # Put computer to sleep
-    def sleep(self):
-        if self.OS == "Windows":
-            os.system("shutdown /h")
+def sleep():
+    if OS == "Windows":
+        os.system("shutdown /h")
 
-        elif self.OS == "MacOS" or self.OS == "Linux":
-            os.system("sudo shutdown -s now")
+    elif OS == "Darwin" or OS == "Linux":
+        os.system("sudo shutdown -s now")
 
 #Logout of computer
-    def logout(self):
-        if self.OS == "Windows":
-            os.system("shutdown /l")
+def logout():
+    if OS == "Windows":
+        os.system("shutdown /l")
 
-        elif self.OS == "MacOS" or self.OS == "Linux":
-            os.system("sudo -s")
+    elif OS == "Darwin" or OS == "Linux":
+        os.system("sudo -s")
 
 # Get IP Address of computer
-    def ip_addr(self):
-        if self.OS == "Windows":
-            hostname = socket.gethostname()
-            ip_address = socket.gethostbyname(hostname)
-            return ip_address
+def ip_address():
+    if OS == "Windows":
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(hostname)
+        return ip
 
-        elif self.OS == "Linux":
-            run = os.popen("hostname -I")
-            output = run.read()
-            ip = ""
-            for i in output:
-                if i != " ":
-                    ip += i
-                elif i == " ":
-                    break
-            return ip.strip()
+    elif OS == "Linux":
+        run = os.popen("hostname -I")
+        output = run.read()
+        ip = ""
+        for i in output:
+            if i != " ":
+                ip += i
+            elif i == " ":
+                break
+        return ip.strip()
 
-        elif self.OS == "MacOS":
-            run = os.popen("ipconfig getifaddr en1")
-            output = run.read()
-            return output.strip()
+    elif OS == "Darwin":
+        run = os.popen("ipconfig getifaddr en1")
+        output = run.read()
+        return output.strip()
